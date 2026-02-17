4cf01212e79b1d96f3032af063174cf6ee881877
oscarrobloxiano <juangarcia16032006@gmail.com>
2026-02-17 17:14:48 -0600

    cuando tengan sus clases actualizo el mio REST

--- src/java/com/zentrix/cadire/rest/RESTCatalogo.java
+++ src/java/com/zentrix/cadire/rest/RESTCatalogo.java
@@ -8,6 +8,33 @@ package com.zentrix.cadire.rest;
  *
  * @author garci
  */
+import com.zentrix.cadire.controller.Catalogo;
+import com.zentrix.cadire.model.Empresa;
+import java.util.List;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("catalogo")
 public class RESTCatalogo {
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+
+        Catalogo c = new Catalogo();
+        List<Empresa> lista = c.getAll();
+
+        return Response.ok(lista).build();
+    }
+    @GET
+    @Path("material/{nombre}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarPorMaterial(@PathParam("nombre") String nombre) {
+
+        Catalogo c = new Catalogo();
+        List<Empresa> lista = c.buscarPorMaterial(nombre);
+
+        return Response.ok(lista).build();
+    }
 }
