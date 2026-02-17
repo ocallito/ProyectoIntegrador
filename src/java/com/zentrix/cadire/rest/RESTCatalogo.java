daaa359df1d29d20785fda77a596aee066dcc8af
oscarrobloxiano <juangarcia16032006@gmail.com>
2026-02-17 16:56:06 -0600

    Aver si sepo

---
+++ src/java/com/zentrix/cadire/rest/RESTCatalogo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zentrix.cadire.rest;
+
+/**
+ *
+ * @author garci
+ */
+public class RESTCatalogo {
+    
+}
