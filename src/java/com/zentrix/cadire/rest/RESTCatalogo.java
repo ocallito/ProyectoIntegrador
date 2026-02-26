877aa40ecbe604bad136c966b30bb52a6377178c
oscarrobloxiano <juangarcia16032006@gmail.com>
2026-02-25 20:07:52 -0600

    aqui estan mis rest

--- src/java/com/zentrix/cadire/rest/RESTCatalogo.java
+++ src/java/com/zentrix/cadire/rest/RESTCatalogo.java
@@ -4,11 +4,11 @@
  */
 package com.zentrix.cadire.rest;
 
-/**
- *
- * @author garci
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import com.zentrix.cadire.controller.Catalogo;
+import com.zentrix.cadire.db.ConexionMySQL;
 import com.zentrix.cadire.model.Empresa;
 import java.util.List;
 import jakarta.ws.rs.*;
@@ -37,4 +37,54 @@ public class RESTCatalogo {
 
         return Response.ok(lista).build();
     }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Empresa insertar(Empresa e) {
+
+    try {
+        ConexionMySQL connMySQL = new ConexionMySQL();
+        Connection conn = connMySQL.open();
+
+        String query = "INSERT INTO empresa(nombre, direccion, telefono, material, precio_kg, ciudad, lat, lng) VALUES(?,?,?,?,?,?,?,?)";
+
+        PreparedStatement ps = conn.prepareStatement(query);
+
+        ps.setString(1, e.getNombre());
+        ps.setString(2, e.getDireccion());
+        ps.setString(3, e.getTelefono());
+        ps.setString(4, e.getMaterial());
+        ps.setDouble(5, e.getPrecioKg());
+        ps.setString(6, e.getCiudad());
+        ps.setDouble(7, e.getLat());
+        ps.setDouble(8, e.getLng());
+
+        ps.executeUpdate();
+        connMySQL.close();
+
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+
+    return e;
+}
+    @DELETE
+    @Path("{id}")
+    public void eliminar(@PathParam("id") int id) {
+
+        try {
+            ConexionMySQL connMySQL = new ConexionMySQL();
+            Connection conn = connMySQL.open();
+
+            String query = "DELETE FROM empresa WHERE id = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+            connMySQL.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+}
 }
---
+++ src/java/com/zentrix/cadire/rest/RESTUsuario.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zentrix.cadire.rest;
+
+import com.zentrix.cadire.db.ConexionMySQL;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.sql.*;
+import com.google.gson.Gson;
+import com.zentrix.cadire.controller.ControllerUsuario;
+import com.zentrix.cadire.model.Usuario;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+ 
+@Path("usuario")
+public class RESTUsuario {
+    @Path("login")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(@FormParam("nombre") @DefaultValue("") String nombre,
+                          @FormParam("contrasenia") @DefaultValue("") String contrasenia)
+    {
+        String out = null;
+        ControllerUsuario cu = new ControllerUsuario();
+        Usuario u = null;
+        
+        try
+        {
+            u = cu.validate(nombre, contrasenia);
+            if(u == null){
+                out = """
+                      {"error" : "Nombre de usuario o contraseña incorrectos."}
+                      """;
+            }else{
+                out = new Gson().toJson(u);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            out = """
+                  {"exception" : "%s"}
+                  """;
+            out = String.format(out, e.toString());
+        }
+        return Response.status(Response.Status.OK).entity(out).build();
+    }
+}
+ 
